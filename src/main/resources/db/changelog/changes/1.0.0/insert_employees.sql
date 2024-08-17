7ad92895338380aba4859251da28ee99570d526b
Andy-mini <lining.arthur@gmail.com>
2024-08-17 11:50:26 +0200

    Use liquibase to update database(add task table, alter task table, insert values to employee table)

---
+++ src/main/resources/db/changelog/changes/1.0.0/insert_employees.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee
+    (first_name, last_name, email)
+VALUES
+    ('John', 'Doe', 'jdoe@test.com'),
+    ('Alice', 'Mary', 'amary@test.com'),
+    ('Bob', 'Cook', 'bcook@test.com');
